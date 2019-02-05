ca4ec3a48ffdabd0379250d1bfbe140b7c7d234f
kalsalehi <kalsalehi@unomaha.edu>
2019-02-04 21:12:25 -0500

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,14 @@ from django.shortcuts import render
 
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
+from django.shortcuts import render
+from .models import Post
+ 
+
+
+Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+ return render(request, 'blog/post_list.html', {})
+
+
+
